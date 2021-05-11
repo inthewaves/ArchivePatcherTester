760c094185865868eb2e65b1aff9a31dd9b39b88
Paul Ngo <inthewaves@pm.me>
2021-05-10 19:50:01 -0700

    show asset files earlier and in the UI

--- app/src/main/java/com/example/archivepatchertester/MainActivity.kt
+++ app/src/main/java/com/example/archivepatchertester/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.lifecycle.lifecycleScope
 import com.google.archivepatcher.applier.FileByFileV1DeltaApplier
 import com.google.common.io.BaseEncoding
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.File
@@ -21,12 +22,17 @@ import java.util.zip.GZIPInputStream
 class MainActivity : AppCompatActivity() {
     private lateinit var middleTextView: TextView
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         middleTextView = findViewById(R.id.middleTextView)
 
         lifecycleScope.launch(Dispatchers.IO) {
+            val assetFiles = assets.list("")?.toList()
+            middleTextView.text = "Assets contain these files: $assetFiles"
+            delay(1500L)
+
             val version25File = File(cacheDir, "Auditor-25.apk")
             assets.open("Auditor-25.apk").use { inStream ->
                 version25File.outputStream().use { outStream ->
@@ -41,11 +47,10 @@ class MainActivity : AppCompatActivity() {
                 DigestOutputStream(version26File.outputStream(), MessageDigest.getInstance("SHA-256")).use { outStream ->
                     inStream.copyTo(outStream)
                     outStream.flush()
-                    version26Digest = BaseEncoding.base16().encode(outStream.messageDigest.digest()).toLowerCase(Locale.ROOT)
+                    version26Digest = BaseEncoding.base16().encode(outStream.messageDigest.digest())
+                        .toLowerCase(Locale.ROOT)
                 }
             }
-
-            Log.d(TAG, "assets contains these files: ${assets.list("")?.toList()}")
 
             val patchFile = File(cacheDir, "auditor-25-to-26-patch-gz")
             assets.open("auditor-25-to-26-patch-gz").use { inStream ->
@@ -84,7 +89,7 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this@MainActivity, "Size of apk is ${version25File.length()}", Toast.LENGTH_LONG).show()
 
                 Log.d(TAG, "The cache dir: ${cacheDir.listFiles()?.toList()}")
-                binding.middleTextView.text = "Gemerating patch. The cache dir: ${cacheDir.listFiles()?.toList()}"
+                middleTextView.text = "Gemerating patch. The cache dir: ${cacheDir.listFiles()?.toList()}"
             }
 
             check(version25File.exists() && version26File.exists())
@@ -94,7 +99,6 @@ class MainActivity : AppCompatActivity() {
             }
             */
             withContext(Dispatchers.Main) {
-                @SuppressLint("SetTextI18n")
                 middleTextView.text = """
                     Done! The cache dir:  ${cacheDir.listFiles()?.toList()}       
                                  
